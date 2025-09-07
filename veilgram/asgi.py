70a27ff711bf13b9f78c392c24226b6fe8d796b3
myeonghan-nim <hannim0828@gmail.com>
2025-09-07 20:45:55 +0900

    Add: ASGI settings

--- veilgram/asgi.py
+++ veilgram/asgi.py
@@ -10,7 +10,21 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'veilgram.settings')
 
-application = get_asgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "veilgram.settings")
+
+django_asgi_app = get_asgi_application()
+
+
+from realtime.routing import websocket_urlpatterns  # noqa: E402
+from realtime.auth import JWTAuthMiddleware  # noqa: E402
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(JWTAuthMiddleware(URLRouter(websocket_urlpatterns))),
+    }
+)
