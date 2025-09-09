822f909b96881bb32cf559433625b7bf282ace17
myeonghan-nim <hannim0828@gmail.com>
2025-09-09 21:55:16 +0900

    Change: integrated URLs

--- veilgram/urls.py
+++ veilgram/urls.py
@@ -20,16 +20,16 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/", include("users.urls")),
-    path("api/v1/", include("profiles.urls")),
-    path("api/v1/", include("relations.urls")),
     path("api/v1/", include("assets.urls")),
     path("api/v1/", include("comments.urls")),
     path("api/v1/", include("feed.urls")),
     path("api/v1/", include("hashtags.urls")),
+    path("api/v1/", include("moderation.urls")),
     path("api/v1/", include("polls.urls")),
     path("api/v1/", include("posts.urls")),
-    path("api/v1/", include("hashtags.urls")),
-    path("api/v1/", include("search.urls")),
+    path("api/v1/", include("profiles.urls")),
+    path("api/v1/", include("relations.urls")),
     path("api/v1/", include("reports.urls")),
+    path("api/v1/", include("search.urls")),
+    path("api/v1/", include("users.urls")),
 ]
